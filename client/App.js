6f053e605b00f3fe37dc202ee00d67bf724afbe4
Alex Panov <thealexpanov@gmail.com>
2018-01-04 13:53:36 -0500

    Keep client components together

--- client/App.js
+++ client/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
 import Todos from './pages/Todos';
-import {Container} from '/imports/client/components/layout';
+import {Container} from '/client/components/layout';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {todos} from '/imports/paths';
-import Menu from '/imports/client/components/Menu';
+import Menu from '/client/components/Menu';
 import {createStore} from 'redux';
 import reducer from './state';
 import {Provider} from 'react-redux';
